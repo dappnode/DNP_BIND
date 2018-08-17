6a8703108caa05c5f030b1a6867d5b927b47aea2
Unknown <guillem.lion.benosa@gmail.com>
2018-08-17 17:19:47 +0200

    Fix network name
    Beware in the test docker-compose that travis will do a docker-compose up in a the directory DNPBIND. Therefore the name of the network has to be dnpbind_network

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,7 @@
+# Beware in the test docker-compose that travis 
+# will do a docker-compose up in a the directory DNPBIND
+# Therefore the name of the network has to be dnpbind_network
+
 docker-compose -f docker-compose-bind.yml build
 docker-compose -f test/docker-compose-test.yml build
 
