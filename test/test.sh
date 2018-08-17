214927da0a335167ac3c75e7bf03513c4c343d18
Unknown <guillem.lion.benosa@gmail.com>
2018-08-17 18:45:11 +0200

    Simplify script

--- test/test.sh
+++ test/test.sh
@@ -2,8 +2,5 @@
 # will do a docker-compose up in a the directory DNPBIND
 # Therefore the name of the network has to be dnpbind_network
 
-docker-compose -f docker-compose-bind.yml build
-docker-compose -f test/docker-compose-test.yml build
-
 docker-compose -f docker-compose-bind.yml up -d
 docker-compose -f test/docker-compose-test.yml run test
