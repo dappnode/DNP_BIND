52195ce97604416c3492a35ea77133bd19263363
Unknown <guillem.lion.benosa@gmail.com>
2018-08-17 18:37:55 +0200

    change up to run
    Try to propagate the error upwards

--- test/test.sh
+++ test/test.sh
@@ -5,5 +5,5 @@
 docker-compose -f docker-compose-bind.yml build
 docker-compose -f test/docker-compose-test.yml build
 
-docker-compose -f docker-compose-bind.yml up -d 
-docker-compose -f test/docker-compose-test.yml up
+docker-compose -f docker-compose-bind.yml up -d
+docker-compose -f test/docker-compose-test.yml run test
