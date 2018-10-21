45b49302a2c0adfa53a24bedd582e1af15a94dbb
Albert Palau <albert.palau@oracle.com>
2018-10-21 12:33:30 +0200

    Work commited https://gitcoin.co/issue/dappnode/DAppNode/31/1421

--- test/build-test/entrypoint.sh
+++ test/build-test/entrypoint.sh
@@ -5,19 +5,14 @@ check_domain()
     DOMAIN=$1
     EXPECTED_IP=$2
     IP=$(dig "$DOMAIN" +short)
-    if [ "$IP" = "$EXPECTED_IP" ]; then
+    if [ "$IP" == "$EXPECTED_IP" ]; then
         echo "resolved $DOMAIN to: $IP"
     else
         echo "Error $DOMAIN resolves to: $IP"
-#        exit 1
+        exit 1
     fi
 }
 
-echo "nameserver 172.33.1.2" > /etc/resolv.conf
-cat /etc/resolv.conf
-echo "ADDRESS:"$(dig @172.33.1.2 ethchain.dnp.dappnode.eth +short)
-ping www.google.es -c 1
-#ping dotbitkittypix.bit -c 1
 check_domain my.ethchain.dnp.dappnode.eth 172.33.1.6
 check_domain ethchain.dappnode.eth 172.33.1.3
 check_domain ethchain.eth 172.33.1.3
