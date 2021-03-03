204ab1250d5a75f71dbfb1b42940641ddc17e0b6
Luka Gulin <lg@shardlabs.io>
2021-03-03 18:30:35 +0000

    No caching

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,28 +1,25 @@
 #!/bin/ash
+INTERNAL_IP=""
+DOMAIN=""
+echo "Trying to fetch domain and internal IP..."
+for i in {1..30}; do
+    if [ -z $INTERNAL_IP  ]; then
+        INTERNAL_IP=$(curl -s http://my.dappnode/global-envs/INTERNAL_IP)
+    fi
 
-if [ ! -f "cloaking-rules.txt" ]; then
-    INTERNAL_IP=""
-    DOMAIN=""
-    echo "Trying to fetch domain and internal IP..."
-    for i in {1..30}; do
-        if [ -z $INTERNAL_IP  ]; then
-            INTERNAL_IP=$(curl -s http://my.dappnode/global-envs/INTERNAL_IP)
-        fi
+    if [ -z $DOMAIN ]; then
+        DOMAIN=$(curl -s http://my.dappnode/global-envs/DOMAIN)
+    fi
 
-        if [ -z $DOMAIN ]; then
-            DOMAIN=$(curl -s http://my.dappnode/global-envs/DOMAIN)
-        fi
-        
-        if [ -z $INTERNAL_IP ] || [ -z $DOMAIN ]; then
-            echo "Retrying in one second..."
-            sleep 1
-        else
-            echo "Done, found $DOMAIN, $INTERNAL_IP"
-            break
-        fi
-    done
+    if [ -z $INTERNAL_IP ] || [ -z $DOMAIN ]; then
+        echo "Retrying in one second..."
+        sleep 1
+    else
+        echo "Done, found $DOMAIN, $INTERNAL_IP"
+        break
+    fi
+done
 
-    echo "$DOMAIN $INTERNAL_IP" > cloaking-rules.txt # Failure mode is creation of empty file, but DNS proxy will still start
-fi
+echo "$DOMAIN $INTERNAL_IP" > cloaking-rules.txt # Failure mode is creation of empty file, but DNS proxy will still start
 
 dnscrypt-proxy -config ./dnscrypt-proxy.toml
