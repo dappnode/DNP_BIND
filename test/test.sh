73fdfeb9b3bf840f26f3c2f3a6de46bda7b6ace7
Christopher Montes <christopher.montes@smarsh.com>
2022-01-28 19:50:45 -0700

    use 172.30.0.0/16 network instead per RFC1918

--- test/test.sh
+++ test/test.sh
@@ -5,6 +5,6 @@
 # [On xenial]:
 #   The name of the folder is DNP_BIND and the network dnp_bind_network
 
-docker network create --driver bridge --subnet 172.33.0.0/16 dncore_network || echo "dncore_network already exists"
+docker network create --driver bridge --subnet 172.30.0.0/16 dncore_network || echo "dncore_network already exists"
 docker-compose -f docker-compose.yml up -d
 docker-compose -f test/docker-compose-test.yml run test
