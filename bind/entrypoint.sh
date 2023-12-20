c81c47da1e5ae358df59fac5fd32d2e8dd3f6171
dappnodedev <diego@dappnode.io>
2023-12-20 16:14:21 +0100

    Do not exit on fetch failure

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -19,12 +19,16 @@ fetch_env() {
         sleep $wait_time
     done
 
-    echo "Error: Failed to fetch $env_var_name after $retries attempts."
+    echo " [ERROR] Failed to fetch $env_var_name after $retries attempts."
     return 1
 }
 
 # Start DNS server in background right away
 /app/dnscrypt-proxy &
+
+# Initialize domain and internal_ip variables
+domain=""
+internal_ip=""
 
 pid=$!
 
@@ -39,9 +43,7 @@ else
     if [ $? -eq 0 ]; then
         domain=$fetched_domain
     else
-        echo "Failed to fetch DOMAIN"
-        kill $pid
-        exit 1
+        echo "[ERROR] Failed to fetch DOMAIN"
     fi
 fi
 
@@ -54,15 +56,18 @@ else
     if [ $? -eq 0 ]; then
         internal_ip=$fetched_internal_ip
     else
-        echo "Failed to fetch INTERNAL_IP"
-        kill $pid
-        exit 1
+        echo "[ERROR] Failed to fetch INTERNAL_IP"
     fi
 fi
 
-echo "$domain $internal_ip" >cloaking-rules.txt
+# Only write to cloaking-rules.txt if both domain and internal_ip are available
+if [ -n "$domain" ] && [ -n "$internal_ip" ]; then
+    echo "$domain $internal_ip" >cloaking-rules.txt
 
-kill $pid
-wait $pid
+    kill $pid
+    wait $pid
 
-/app/dnscrypt-proxy
+    /app/dnscrypt-proxy
+else
+    echo "[ERROR] Missing domain or internal IP. Cloaking rules not updated. Dyndns domain will not be forwarded to internal IP."
+fi
