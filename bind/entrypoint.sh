c55c393407a6db1c91a4341e253a41ae89f8be46
dappnodedev <diego@dappnode.io>
2024-01-23 16:46:14 +0100

    Infinitely try to fetch envs

--- bind/entrypoint.sh
+++ bind/entrypoint.sh
@@ -4,11 +4,10 @@
 fetch_env() {
     local env_var_name=$1
     local env_var_value=""
-    local retries=30  # Number of retries
-    local wait_time=1 # Wait time between retries
+    local wait_time=10 # Wait time between retries
 
-    for i in $(seq 1 $retries); do
-        echo "Fetching $env_var_name (attempt $i)"
+    while true; do
+        echo "Fetching $env_var_name"
         env_var_value=$(curl -s "my.dappnode/global-envs/$env_var_name")
 
         if [ -n "$env_var_value" ]; then
@@ -18,9 +17,6 @@ fetch_env() {
 
         sleep $wait_time
     done
-
-    echo " [ERROR] Failed to fetch $env_var_name after $retries attempts."
-    return 1
 }
 
 # Start DNS server in background right away
@@ -39,12 +35,7 @@ if [ -n "${_DAPPNODE_GLOBAL_DOMAIN}" ]; then
     echo "Using existing domain: $domain"
 else
     fetched_domain=$(fetch_env "DOMAIN" | tail -n 1)
-
-    if [ $? -eq 0 ]; then
-        domain=$fetched_domain
-    else
-        echo "[ERROR] Failed to fetch DOMAIN"
-    fi
+    domain=$fetched_domain
 fi
 
 if [ -n "${_DAPPNODE_GLOBAL_INTERNAL_IP}" ]; then
@@ -52,12 +43,7 @@ if [ -n "${_DAPPNODE_GLOBAL_INTERNAL_IP}" ]; then
     echo "Using existing internal IP: $internal_ip"
 else
     fetched_internal_ip=$(fetch_env "INTERNAL_IP" | tail -n 1)
-
-    if [ $? -eq 0 ]; then
-        internal_ip=$fetched_internal_ip
-    else
-        echo "[ERROR] Failed to fetch INTERNAL_IP"
-    fi
+    internal_ip=$fetched_internal_ip
 fi
 
 # Only write to cloaking-rules.txt if both domain and internal_ip are available
